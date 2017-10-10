f371072d4566b5dd0d085231f6f92dbc65644f70
rgirard1 <remygirard@hotmail.fr>
2017-10-10 12:28:23 +0200

    clean cmake

--- CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/antoine/git/AtelierRTCam
-# Build directory: /home/antoine/git/AtelierRTCam
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
